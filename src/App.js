7dff5e53c6b941aa053045643510e590437de755
Ayush Kumar Jha <ayushkumarj4@gmail.com>
2023-07-19 01:04:29 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import GithubSearchApp from './components/GithubSearchApp';
+function App() {
+  return (
+      <GithubSearchApp/>
+  );
+}
+
+export default App;
